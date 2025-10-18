931be05f20b3ff3e9ff78b84c397299fa4e528f0
ArTeGaS-2 <probablytorvik@gmail.com>
2025-10-18 19:56:59 +0300

    Елементи інтерфейсу Tkinter: кнопки, лейбли, фрейми

--- Lesson_6/main.py
+++ Lesson_6/main.py
@@ -15,6 +15,30 @@ class GuessingGame:
             text="Комп'ютер загадав число від 1 до 100. Спробуй вгадати:")
         self.info_label.pack(padx=12, pady=(12, 8)) # Зовнішні відступи віджета
 
+        entry_frame = tk.Frame(self.master)
+        entry_frame.pack(padx=12, pady=4)
+
+        tk.Label(entry_frame, text="Ваша спроба: ").pack(side=tk.LEFT)
+
+        self.guess_entry = tk.Entry(entry_frame, width=10, justify="center")
+        self.guess_entry.pack(side=tk.LEFT, padx=(6,0))
+        self.guess_entry.bind("<Return>", self.chech_guess)
+        # Кнопка перевірки
+        self.guess_button = tk.Button(self.master, text="Перевірити",
+                                      command=self.chech_guess)
+        self.guess_button.pack(padx=12, pady=(4, 8))
+        # текст підказка
+        self.feedback_label = tk.Label(self.master,
+            text="Введіть число вище і натисніть кнопку.")
+        self.feedback_label.pack(padx=12, pady=(4, 8))
+        # текст - кількість спроб
+        self.attempts_label = tk.Label(self.master, text="Спроб: 0")
+        self.attempts_label.pack(padx=12, pady=(0, 12))
+        # Кнопка "Нова гра", або перезапуск
+        self.reset_button = tk.Button(self.master, text="Нова гра",
+            command=self.reset_game)
+        self.reset_button.pack(padx=12, pady=(4, 8))
+
     def reset_game(self):
         pass
 
