b2bb033a4a83045eeb297bf5098c397c01ef0ae1
ArTeGaS-2 <probablytorvik@gmail.com>
2026-04-11 21:33:38 +0300

    Продовжили роботу над скриптом побудови мапи

--- TowerDefense/src/map/tile_map.py
+++ TowerDefense/src/map/tile_map.py
@@ -8,3 +8,57 @@ from settings import (
     GRASS_TILE_PATH,
     ROAD_TILE_PATH,
     SPAWN_TILE_PATH)
+
+class TileMap:
+    def __init__(self, json_path):
+        with open(json_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        self.tile_size = data["tile_size"] # розмір плитки
+        self.cols = data["cols"] # стовбчики
+        self.rows = data["rows"] # рядки
+        self.grid = data["grid"] # сітка, з клітинками
+
+        self.tile_images = {
+            0: self.load_tile(GRASS_TILE_PATH),
+            1: self.load_tile(ROAD_TILE_PATH),
+            2: self.load_tile(SPAWN_TILE_PATH),
+            3: self.load_tile(GOAL_TILE_PATH),
+            4: self.load_tile(BUILD_SPOT_TILE_PATH)
+        }
+
+        self.path_points = self.build_path()
+
+    def load_tile(self, image_path):
+        image = pygame.image.load(str(image_path)).convert_alpha()
+        return pygame.transform.scale(image, (self.tile_size, self.tile_size))
+    
+    def draw(self, surface):
+        for row_index, row in enumerate(self.grid):
+            for col_index, tile_value in enumerate(row):
+                image = self.tile_images[tile_value]
+                x = col_index * self.tile_size
+                y = row_index * self.tile_size
+                surface.blit(image, (x,y))
+
+    def build_path(self):
+        current = self.find_tile(2)
+        previous = None
+        path_tiles = [current]
+
+        while self.get_tile_value(current) != 3:
+            next_tile = self.find_next_path_tile(current, previous)
+            path_tiles.append(next_tile)
+            previous = current
+            current = next_tile
+
+        return [self.get_tile_center(tile) for tile in path_tiles]
+    
+    def find_tile(self, target_value):
+        for row_index, row in enumerate(self.grid):
+            for col_index, tile_value in enumerate(row):
+                if tile_value == target_value:
+                    return col_index, row_index
+        
+        raise ValueError(f"Tile with value {target_value} was not found.")
+
